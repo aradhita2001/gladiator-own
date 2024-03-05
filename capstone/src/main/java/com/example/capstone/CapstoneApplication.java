bbed96232025470711d1fd4cf25f5729be4d7e51
username <wecp-dummy@gmail.com>
2024-03-05 03:55:58 +0000

    create initial admin

--- capstone/src/main/java/com/example/capstone/CapstoneApplication.java
+++ capstone/src/main/java/com/example/capstone/CapstoneApplication.java
@@ -1,13 +1,34 @@
 package com.example.capstone;
-
+ 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+ 
+import com.example.capstone.entity.User;
+import com.example.capstone.exception.UserAlreadyExistsException;
+import com.example.capstone.service.UserService;
+ 
 @SpringBootApplication
-public class CapstoneApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(CapstoneApplication.class, args);
-	}
-
+public class CapstoneApplication implements CommandLineRunner{
+ 
+    @Autowired
+    private UserService userService;
+ 
+    public static void main(String[] args) {
+        SpringApplication.run(CapstoneApplication.class, args);
+    }
+ 
+    @Override
+    public void run(String... args) throws Exception {
+        User admin = new User("Aradhita", "aradhita@email.com", "9876543210", "ADMIN", "Pass@1234", null);
+        admin.setUserId(1);
+        try {
+        userService.addUser(admin);
+        } catch (UserAlreadyExistsException e) {
+            System.out.println("Master Admin already exists");
+        }
+    }
+ 
 }
+ 
